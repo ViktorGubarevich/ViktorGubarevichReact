65f44018b20315331bba33b759e1247bc479cf99
Viktor Gubarevich <gubarew.v@gmail.com>
2021-02-28 23:35:03 +0300

    refactor: update index.js, App.js, Counter.js

--- my_first_app/src/components/App.js
+++ my_first_app/src/components/App.js
@@ -1,38 +1,60 @@
 import React from "react";
-import './App.scss';
+import Counter from "./Counter";
+import "./App.scss";
 import plus from "../images/plus.png";
 import minus from "../images/minus.png";
 import update from "../images/update.png";
 
-function App() {
-  return ( < div className = 'main' >
-    <
-    div className = 'app' >
-    <
-    div className = 'count' > 13 < /div> <
-    div className = 'buttons' >
-    <
-    div className = 'btn' > < button className = 'button-plus' > < img className = "img-plus"
-    src = {
-      plus
-    }
-    alt = "minus" / > < /button></div >
-    <
-    div className = 'btn' > < button className = 'button-update' > < img className = "img-update"
-    src = {
-      update
-    }
-    alt = "minus" / > < /button> < /
-    div > <
-    div className = 'btn' > < button className = 'button-minus' > < img className = "img-minus"
-    src = {
-      minus
-    }
-    alt = "minus" / > < /button> < /
-    div > <
-    /div> < /
-    div > <
-    /div>);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
   }
 
-  export default App;
+  clickPlus = () => {
+    this.setState((state) => ({ counter: state.counter + 1 }));
+  };
+
+  clickUpdate = () => {
+    this.setState((state) => ({ counter: (state.counter = 0) }));
+  };
+
+  clickMinus = () => {
+    this.setState((state) => ({ counter: state.counter - 1 }));
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <div className="app">
+          <Counter counter={this.state.counter} />
+          <div className="buttons">
+            <div className="btn">
+              {" "}
+              <button className="button-plus" onClick={this.clickPlus}>
+                {" "}
+                <img className="img-plus" src={plus} alt="plus" />{" "}
+              </button>{" "}
+            </div>{" "}
+            <div className="btn">
+              {" "}
+              <button className="button-update" onClick={this.clickUpdate}>
+                {" "}
+                <img className="img-update" src={update} alt="update" />{" "}
+              </button>{" "}
+            </div>{" "}
+            <div className="btn">
+              {" "}
+              <button className="button-minus" onClick={this.clickMinus}>
+                {" "}
+                <img className="img-minus" src={minus} alt="minus" />{" "}
+              </button>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+export default App;
