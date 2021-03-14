e30cb59536a5810247474ab69c7a9d80fc88ec15
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-14 21:44:44 +0300

    feat: add converter

--- my-second-app/src/App.js
+++ my-second-app/src/App.js
@@ -1,7 +1,117 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import { Fieldset } from "./Fieldset";
 
-function App() {
-  return <p>Hello World </p>;
+// const rate = 2.64;
+
+// function App() {
+
+//   const [onChangeInUSD, setChangeInUSD] = useState(0);
+//   const [onChangeInBYN, setChangeInBYN] = useState(0);
+
+//   function setChangeInUSD = (value) => {
+//     onChangeInUSD({ currency: "USD", value });
+//   };
+//   function setChangeInBYN = (value) => {
+//     onChangeInBYN({ currency: "BYN", value });
+//   };
+
+//   toBYN = (USD) => {
+//     return USD * rate;
+//   };
+
+//   toUSD = (BYN) => {
+//     return BYN / rate;
+//   };
+
+//   convertCurrency = (value, convertor) => {
+//     let converted = convertor(value);
+//     let rounded = Math.round(converted * 100) / 100;
+//     return rounded;
+//   };
+
+//   return (
+
+//     const value = {value};
+//     const currency = {currency};
+//     const USD =
+//       currency === "BYN" ? this.convertCurrency(value, toUSD) : value;
+//     const BYN =
+//       currency === "USD" ? this.convertCurrency(value, toBYN) : value;
+
+//     <form className="app">
+//       <h2>Live currency converter</h2>
+//       <Fieldset
+//         legend="$, USD"
+//         currency="USD"
+//         value={USD}
+//         onChange={setChangeInUSD}
+//       />
+//       <Fieldset
+//         currency="BYN"
+//         legend="BYN"
+//         value={BYN}
+//         onChange={setChangeInBYN}
+//       />
+//     </form>
+//   );
+// }
+
+const rate = 2.64;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
+
+  toBYN = (USD) => {
+    return USD * rate;
+  };
+
+  toUSD = (BYN) => {
+    return BYN / rate;
+  };
+
+  convertCurrency = (value, convertor) => {
+    let converted = convertor(value);
+    let rounded = Math.round(converted * 100) / 100;
+    return rounded;
+  };
+
+  onChangeInUSD = (value) => {
+    this.setState({ currency: "USD", value });
+  };
+  onChangeInBYN = (value) => {
+    this.setState({ currency: "BYN", value });
+  };
+
+  render() {
+    const value = this.state.value;
+    const currency = this.state.currency;
+    const USD =
+      currency === "BYN" ? this.convertCurrency(value, this.toUSD) : value;
+    const BYN =
+      currency === "USD" ? this.convertCurrency(value, this.toBYN) : value;
+
+    return (
+      <form className="app">
+        <h2>Live currency converter</h2>
+        <Fieldset
+          legend="$, USD"
+          currency="USD"
+          value={USD}
+          onChange={this.onChangeInUSD}
+        />
+        <Fieldset
+          currency="BYN"
+          legend="BYN"
+          value={BYN}
+          onChange={this.onChangeInBYN}
+        />
+      </form>
+    );
+  }
 }
 
 export default App;
---
+++ my-second-app/src/Fieldset.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Fieldset.scss";
+
+function Fieldset(props) {
+  const inputChange = (event) => props.onChange(event.target.value);
+  const value = props.value;
+  const currency = props.currency;
+
+  return (
+    <fieldset className="app-exchanger">
+      <legend className="legend">Enter currency in {props.legend}</legend>
+      <label for={currency}> {currency} </label>
+      <input
+        id={currency}
+        type="number"
+        value={value}
+        onChange={inputChange}
+      ></input>
+    </fieldset>
+  );
+}
+
+export { Fieldset };
